0a061c5e6530792c46a71fab95fd1a0d8ce23fbb
Daniel Heppner <danielhep@Daniels-MacBook-Pro.local>
2020-06-07 00:19:57 -0700

    round dep time

--- src/realtime/realtime.js
+++ src/realtime/realtime.js
@@ -14,7 +14,7 @@ module.exports = {
       predicted: x.predicted[0] === 'true',
       // predicted time == 0 when it's a long way out?
       // the times are returned as strings, so this code is messy because we are doing implicit casts. TODO
-      predictedDepartureTime: x.predicted[0] === 'true' ? (x.predictedDepartureTime[0] - currentTime) / 1000 / 60 : (x.scheduledDepartureTime[0] - currentTime) / 1000 / 60,
+      predictedDepartureTime: x.predicted[0] === 'true' ? Math.round((x.predictedDepartureTime[0] - currentTime) / 1000 / 60) : Math.round((x.scheduledDepartureTime[0] - currentTime) / 1000 / 60),
       deviation: x.predicted[0] === 'true' ? x.tripStatus[0].scheduleDeviation[0] / 60 : null
     }))
     console.log(stopTimes[0].tripStatus)
